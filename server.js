1b75f854cd188aee7fc1b7bdbaa92220d46cfbf2
jdbanuel <jamesdb1995@gmail.com>
2019-10-01 20:10:28 -0700

    Connecting  to Mongo Atlas DB
    Adding default routes for endpoints

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
-
+const connectDB = require('./config/db.js');
 const app = express();
 
-app.get('/', (req, res) => res.send('API Running')); 
+//Connecting Database
+connectDB();
+
+app.get('/', (req, res) => res.send('API Running'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/comments', require('./routes/api/comments'));
+app.use('/api/blogposts', require('./routes/api/blogposts'));
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {console.log(`Server started on port ${PORT}`)});
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+});
