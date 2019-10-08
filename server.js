f566be66e233af880e8df19727e3857f785d3e1c
jdbanuel <jamesdb1995@gmail.com>
2019-10-07 20:58:05 -0700

    Adding profile route to server.js

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const app = express();
 connectDB();
 
 //Setting up body-parser
-app.use(express.json({
-    extended: false
-}));
+app.use(
+	express.json({
+		extended: false
+	})
+);
 
 app.get('/', (req, res) => res.send('API Running'));
 
@@ -17,9 +19,10 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/comments', require('./routes/api/comments'));
 app.use('/api/blogposts', require('./routes/api/blogposts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`)
+	console.log(`Server started on port ${PORT}`);
 });
