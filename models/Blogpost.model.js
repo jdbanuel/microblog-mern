739d5b4d0daa17ae4ee22a5bfcdd54b9527ef944
jdbanuel <jamesdb1995@gmail.com>
2019-10-17 21:35:06 -0700

    Adding comments field to a blogpost

--- models/Blogpost.model.js
+++ models/Blogpost.model.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose');
 
 const BlogpostSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    text: String,
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    author: {
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
-        },
-        userName: String
-    }
+	title: String,
+	description: String,
+	text: String,
+	createdAt: {
+		type: Date,
+		default: Date.now
+	},
+	author: {
+		id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User'
+		},
+		userName: String
+	},
+	comments: {
+		type: [mongoose.Schema.Types.ObjectId],
+		ref: 'Comment'
+	}
 });
 
-module.exports = mongoose.model("Blogpost", BlogpostSchema);
+module.exports = mongoose.model('Blogpost', BlogpostSchema);
